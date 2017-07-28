7f3be180033eafe5200fd1a201ab84b2d5179931
Craig M. Stimmel <cstimmel@uw.edu>
2017-07-28 15:01:14 -0700

    wrap the api views

--- scout_server/urls.py
+++ scout_server/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from scout_server import views
 
 urlpatterns = [
     url(r'^spots/$', views.spot_list),
     url(r'^spots/(?P<pk>[0-9]+)/$', views.spot_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- scout_server/views.py
+++ scout_server/views.py
@@ -1,53 +1,50 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from scout_server.models import Spot
 from scout_server.serializers import SpotSerializer
 
 
-# Create your views here.
-@csrf_exempt
-def spot_list(request):
+@api_view(['GET', 'POST'])
+def spot_list(request, format=None):
 	if request.method == 'GET':
 		spots = Spot.objects.all()
 		serializer = SpotSerializer(spots, many=True)
-		return JsonResponse(serializer.data, safe=False)
+		return Response(serializer.data)
 
 	if request.method == 'POST':
 		data = JSONParser().parse(request)
 		serializer = SpotSerializer(data=data)
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data, status=201)
-		return JsonResponse(serializer.errors, status=400)
+			return Response(serializer.data, status=201)
+		return Response(serializer.errors, status=400)
 
-@csrf_exempt
-def spot_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def spot_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
     try:
         spot = Spot.objects.get(pk=pk)
     except Spot.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = SpotSerializer(spot)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = SpotSerializer(spot, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         spot.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
