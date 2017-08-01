974b2cde4df259c149b161ab97e57b60296437d8
Craig M. Stimmel <cstimmel@uw.edu>
2017-08-01 15:46:14 -0700

    Make spotextendedinfo writable.

--- scout_server/serializers.py
+++ scout_server/serializers.py
@@ -9,9 +9,16 @@ class SpotExtendedInfoSerializer(serializers.ModelSerializer):
 
 
 class SpotSerializer(serializers.ModelSerializer):
-    spotextendedinfo = SpotExtendedInfoSerializer(many=True, read_only=True)
+    spotextendedinfo = SpotExtendedInfoSerializer(many=True)
 
     class Meta:
         model = Spot
         fields = ('id', 'name', 'latitude', 'longitude', 'building_name', 'floor', 'room_number', 'capacity', 'display_access_restrictions', 'organization', 'manager', 'last_modified', 'external_id', 'spotextendedinfo')
+
+    def create(self, validated_data):
+        spotextendedinfo_data = validate_data.pop('spotextendedinfo')
+        spot = Spot.objects.create(**validated_data)
+        for spotextendedinfo_datum in spotextendedinfo_data:
+            SpotExtendedInfo.objects.create(spot=spot, **spotextendedinfo_data)
+        return spot
 
