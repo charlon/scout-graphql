fc0a7b4ac6671d8f181f4d0a44a0b4766cffe5b7
Charlon Palacay <charlon@gmail.com>
2018-01-26 15:14:04 -0800

    Sets ALLOWED_HOSTS via env variables.

--- serverproject/serverproject/settings/local.py
+++ serverproject/serverproject/settings/local.py
@@ -31,8 +31,7 @@ SECRET_KEY = env('SECRET_KEY', default='CHANGE_ME_IN_ENV_FILE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
 
 # Application definition
 
