f749e962377e2af576569b27a1e1b1b3d3318c07
Charlon Palacay <charlon@gmail.com>
2018-02-27 23:37:26 -0800

    Configure webpack to handle bundle splitting by vendor and component.

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,36 @@ var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
+
+  mode: "development",
   context: __dirname,
 
-  entry: './react/js/app', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+  //entry: './react/js/app', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+
+  entry : {
+    hello: './react/js/hello',
+    world: './react/js/world'
+  },
+
+  optimization: {
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					chunks: "initial",
+					minChunks: 2,
+					maxInitialRequests: 5, // The default limit is too small to showcase the effect
+					minSize: 0 // This is example is too small to create commons chunks
+				},
+				vendor: {
+					test: /node_modules/,
+					chunks: "initial",
+					name: "vendor",
+					priority: 10,
+					enforce: true
+				}
+			}
+		}
+	},
 
   output: {
       path: path.resolve('./react/bundles/'),
