9e977b69b1cb32f509c96cb6ed42f3407c75f96f
Charlon Palacay <charlon@gmail.com>
2018-03-01 11:56:10 -0800

    Updates hello react component.

--- scout_clients/static/scout_clients/react/hello.js
+++ scout_clients/static/scout_clients/react/hello.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = <h1>Hello...</h1>;
+const Hello = <h1>Helloooo...</h1>;
 ReactDOM.render(Hello, document.getElementById('hello'));
