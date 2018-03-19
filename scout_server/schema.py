545b8fa6506434b5fa9af6f21a406b0bd8360313
Craig M. Stimmel <cstimmel@uw.edu>
2018-03-19 13:25:58 -0700

    Add photos query.

--- scout_server/schema.py
+++ scout_server/schema.py
@@ -1,14 +1,42 @@
 import graphene
+import json
+import requests
+from collections import namedtuple
 
 from graphene_django.types import DjangoObjectType, ObjectType
 
 from scout_server.models import Spot
 
+
+def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
+
+class PhotoObjectType(ObjectType):
+    albumId = graphene.ID()
+    id = graphene.ID()
+    title = graphene.String()
+    url = graphene.String()
+    thumbnailUrl = graphene.String()
+
+
 class SpotObjectType(DjangoObjectType):
     class Meta:
         model = Spot
 
-class Query(object):
+
+class PhotoQuery(object):
+    photos = graphene.List(PhotoObjectType,)
+
+    def resolve_photos(self, info, **kwargs):
+        url = "https://jsonplaceholder.typicode.com/photos"
+        response = requests.get(url=url)
+        data = response.text
+        photos = json2obj(data)
+        return photos
+
+
+class SpotQuery(object):
     spots = graphene.List(SpotObjectType, spot_id=graphene.Int(),)
 
     def resolve_spots(self, info, **kwargs):
@@ -19,3 +47,7 @@ class Query(object):
         else:
             spots = Spot.objects.all()
             return spots
+
+
+class Query(PhotoQuery, SpotQuery):
+    pass
