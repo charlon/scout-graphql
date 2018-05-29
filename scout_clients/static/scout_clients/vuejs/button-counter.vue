2d6d531f54dd0a184e05e1646ad6e0f20bd39c37
Charlon Palacay <charlon@gmail.com>
2018-05-29 13:17:40 -0700

    Adds begin of example vue single file component.

---
+++ scout_clients/static/scout_clients/vuejs/button-counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="margin-bottom:25px;">
+    <button v-on:click="count++">You clicked me {{ count }} times. (single file component)</button>
+  </div>
+</template>
+<script>
+  export default {
+    data: function () {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+<style lang="scss">
+#ButtonCounter {
+  // some scss rules
+}
+</style>
