44bc3f61c0d97ab84d87a34d7819e8c582ed86a1
Charlon Palacay <charlon@gmail.com>
2018-06-08 16:51:08 -0700

    Allowed hosts hack to make it work anywhere.

--- docker/settings.py
+++ docker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'CHANGE_ME_IN_IM_SECRET'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
