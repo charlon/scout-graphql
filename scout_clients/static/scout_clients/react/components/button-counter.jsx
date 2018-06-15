87c8249ee2eda75edf1c0f10b3ebdead33fa1063
Charlon Palacay <charlon@gmail.com>
2018-06-15 12:14:01 -0700

    Add reactjs text string as props.

--- scout_clients/static/scout_clients/react/components/button-counter.jsx
+++ scout_clients/static/scout_clients/react/components/button-counter.jsx
@@ -3,20 +3,23 @@ import React, { Component } from 'react';
 export class ButtonCounter extends React.Component {
 
   constructor(props) {
-        super(props);
-        this.state = { count: 0 };
-        this.incrementCount = this.incrementCount.bind(this);
-    }
+    super(props);
+    this.state = {
+                  count: 0,
+                  blah: "This is extra text!"
+                 };
+    this.incrementCount = this.incrementCount.bind(this);
+  }
 
-    incrementCount(){
-      this.setState({count: this.state.count + 1});
-    }
+  incrementCount(){
+    this.setState({count: this.state.count + 1});
+  }
 
-    render() {
-        return (
-          <div>
-            <button onClick={ this.incrementCount }>You clicked me {this.state.count} times.. (exported react component)</button>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <button onClick={ this.incrementCount }>You clicked me {this.state.count} times. {this.state.blah} (exported react component)</button>
+      </div>
+    );
+  }
 };
