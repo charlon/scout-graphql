544e2a21da91d8e09562f72c71d38e4f791b7e40
Charlon Palacay <charlon@gmail.com>
2018-07-27 12:46:12 -0700

    Better implimentation of spot lists with loading message.

--- scout_clients/static/scout_clients/react/components/spots-list.jsx
+++ scout_clients/static/scout_clients/react/components/spots-list.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from "axios";
 
 const styles = {
   marginBottom: '25px'
@@ -10,38 +11,59 @@ export class SpotsList extends React.Component {
     super(props);
     this.state = {
                   title: 'All Spooots',
-                  spots: window.props
+                  loading: true,
+                  spots: []
                  };
+  }
+
+  componentDidMount() {
+    // because the data is already available, delay the display to make it
+    // appear faster
+    setTimeout(
+        function() {
+          this.setState({
+            spots: window.props,
+            loading: false
+          });
+        }
+        .bind(this),
+        200
+    );
   }
 
   render() {
 
-    //console.log(this.state.spots);
-    console.log("spots list rendered!");
+    let content;
+
+    if (this.state.loading) {
+      content = <div className="pt-3">Loading...</div>;
+    } else {
+      content =
+        <ul className="p-0">
+          {this.state.spots.map( spot => {
+            return (
+              <li key={spot.id} className="media text-muted pt-3">
+                   <img src="http://via.placeholder.com/32x32" alt="..." className="mr-2 rounded" />
+                   <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                     <strong className="d-block text-gray-dark">{spot.name }</strong>
+                     {spot.building_name }<br/>
+                     {spot.latitude }, {spot.longitude }</p>
+              </li>
+            );
+          })}
+        </ul>
+    }
 
     return (
-      <div>
-        <div>
-          <small className="text-muted">The following component...</small>
-        </div>
-
-        <div style={styles} className="my-3 p-3 bg-white rounded box-shadow">
-          <h6 className="border-bottom border-gray pb-2 mb-0">All Spots</h6>
-          <ul className="p-0">
-           {this.state.spots.map( spot => {
-             return (
-               <li key={spot.id} className="media text-muted pt-3">
-                 <img src="http://via.placeholder.com/32x32" alt="..." className="mr-2 rounded" />
-                 <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                   <strong className="d-block text-gray-dark">{spot.name }</strong>
-                   {spot.building_name }<br/>
-                   {spot.latitude }, {spot.longitude }</p>
-              </li>
-               );
-             })}
-           </ul>
-        </div>
-      </div>
+     <div style={styles}>
+       <div>
+         <small className="text-muted">The following component...</small>
+       </div>
+       <div style={styles} className="my-3 p-3 bg-white rounded box-shadow">
+         <h6 className="border-bottom border-gray pb-2 mb-0">All Spots</h6>
+         {content}
+       </div>
+     </div>
     );
 
   }
